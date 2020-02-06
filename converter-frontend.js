1c10b8a97e0c2a558aa053cdb0c41420dc922caa
Michael Passade <michael.passade@codeimmersives.com>
2020-02-06 14:10:57 -0500

    edit

--- converter-frontend.js
+++ converter-frontend.js
@@ -1,10 +1,38 @@
 const converter = require('./converter-backend.js')
-
 const firstNumberIndex = process.argv.findIndex((value) => !isNaN(value))
+const firstUnit = process.argv.findIndex((unit) => 
+    unit === 'Fahrenheit'
+    || unit === 'fahrenheit'
+    || unit === 'F'
+    || unit === 'f'
+    || unit === 'Celsius'
+    || unit === 'celsius'
+    || unit === 'C'
+    || unit === 'c'
+    || unit === 'Kelvin'
+    || unit === 'kelvin'
+    || unit === 'K'
+    || unit === 'k'
+    )   
+//let newStr = process.argv.slice(firstUnit+1)
+const secondUnit = process.argv.slice(firstUnit+1).findIndex((unit) => 
+    unit === 'Fahrenheit'
+    || unit === 'fahrenheit'
+    || unit === 'F'
+    || unit === 'f'
+    || unit === 'Celsius'
+    || unit === 'celsius'
+    || unit === 'C'
+    || unit === 'c'
+    || unit === 'Kelvin'
+    || unit === 'kelvin'
+    || unit === 'K'
+    || unit === 'k'
+    )  
 
 const num = process.argv[firstNumberIndex]
-const temp_scale = process.argv[firstNumberIndex+1]
-
-const result = converter(num,temp_scale)
+const temp_scale1 = process.argv[firstUnit]
+const temp_scale2 = process.argv[secondUnit]
+const result = converter(num,temp_scale1,temp_scale2)
 
 console.log(result)
